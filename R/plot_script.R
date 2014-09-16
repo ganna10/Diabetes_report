a5aa597556a462b394bf3272166428f9bc031deb
ganna10 <janecoates10@gmail.com>
2014-09-16 13:47:17 +0200

    Creating repro

---
+++ R/plot_script.R
@@ -0,0 +1,27 @@
+#time series plot of csv data downloaded from dbees.com
+#command line argument is the csv file
+
+args = commandArgs(TRUE)
+input = args[1]
+file = gsub("../csv_files/", "", input)
+output = gsub("csv", "pdf", file)
+
+library("ggplot2")
+library("Cairo")
+
+data = read.csv(file = input, header = TRUE, sep = ";")
+data$Time = paste(data$GIVEN_DATE, data$COL_TIME, sep = " ")
+
+plot = ggplot(data, aes(x = Time, y = GLYCEMIA_READING))
+plot = plot + geom_point(size = 4)
+plot = plot + theme_bw()
+plot = plot + ylab("Glycemy (mg/dl)")
+plot = plot + xlab("")
+plot = plot + theme(axis.text.x = element_text(angle = 90))
+plot = plot + theme(axis.text.y = element_text(size = 18))
+plot = plot + theme(axis.text.x = element_text(size = 14))
+plot = plot + theme(axis.title.y = element_text(face = "bold", size = 24))
+
+CairoPDF(file = output, width = 23, height = 16)
+print(plot)
+dev.off()
---
+++ R/table_script.R
@@ -0,0 +1,19 @@
+#Manupulate csv data downloaded from dbees.com for report
+#command line argument is the csv file
+
+args = commandArgs(TRUE)
+input = args[1]
+file = gsub("csv_files/", "csv_files/out_", input)
+
+data = read.csv(file = input, header = TRUE, sep = ";")
+data[is.na(data)] = c("")
+data$COL_TIME = paste(data$GIVEN_DATE, data$COL_TIME, sep = " ")
+data$GIVEN_DATE = NULL
+data$MEAL_SIZE = NULL
+data$PHYS_SCALE = NULL
+data$WEIGHT_READING = NULL
+colnames(data) =  c("Activity", "Time", "Meal", "Text", "InsulinType", "UnitsInjected", "MealCarbohydrates", "GlycemiaTiming", "GlycemiaReading", "Exercise")
+data$Time = as.POSIXlt(data$Time, format="%Y-%m-%d %H:%M", tz="Europe/Berlin")
+data = data[order(data$Time),]
+
+write.table(data, file = file, row.names = FALSE, quote = FALSE, sep = "|")
